6048883e811e01eaa16e29b513968f18379954a7
Muhammad Waqar <>
2020-03-20 17:49:29 -0400

    ops, a few dates got missed

---
+++ snapshots_from_wayback/f28_files/api.js
@@ -0,0 +1,21 @@
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */(function(){var w=window,C='___grecaptcha_cfg',cfg=w[C]=w[C]||{},N='grecaptcha';var gr=w[N]=w[N]||{};gr.ready=gr.ready||function(f){(cfg['fns']=cfg['fns']||[]).push(f);};(cfg['render']=cfg['render']||[]).push('explicit');w['__google_recaptcha_client']=true;var d=document,po=d.createElement('script');po.type='text/javascript';po.async=true;po.src='https://web.archive.org/web/20200228201835/https://www.gstatic.com/recaptcha/releases/61bII03-TtCmSUR7dw9MJF9q/recaptcha__en.js';var e=d.querySelector('script[nonce]'),n=e&&(e['nonce']||e.getAttribute('nonce'));if(n){po.setAttribute('nonce',n);}var s=d.getElementsByTagName('script')[0];s.parentNode.insertBefore(po, s);})();
+/*
+     FILE ARCHIVED ON 20:18:35 Feb 28, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:48:33 Mar 20, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.198
+  esindex: 0.015
+  CDXLines.iter: 45.788 (3)
+  PetaboxLoader3.datanode: 1099.489 (8)
+  LoadShardBlock: 102.808 (6)
+  exclusion.robots.policy: 0.183
+  load_resource: 1263.165
+  PetaboxLoader3.resolve: 106.697 (2)
+  RedisCDXSource: 7.636
+*/
---
+++ snapshots_from_wayback/m4_files/api.js
@@ -0,0 +1,21 @@
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */(function(){var w=window,C='___grecaptcha_cfg',cfg=w[C]=w[C]||{},N='grecaptcha';var gr=w[N]=w[N]||{};gr.ready=gr.ready||function(f){(cfg['fns']=cfg['fns']||[]).push(f);};(cfg['render']=cfg['render']||[]).push('explicit');w['__google_recaptcha_client']=true;var d=document,po=d.createElement('script');po.type='text/javascript';po.async=true;po.src='https://web.archive.org/web/20200304135340/https://www.gstatic.com/recaptcha/releases/61bII03-TtCmSUR7dw9MJF9q/recaptcha__en.js';var e=d.querySelector('script[nonce]'),n=e&&(e['nonce']||e.getAttribute('nonce'));if(n){po.setAttribute('nonce',n);}var s=d.getElementsByTagName('script')[0];s.parentNode.insertBefore(po, s);})();
+/*
+     FILE ARCHIVED ON 13:53:40 Mar 04, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:47:50 Mar 20, 2020.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 483.55 (6)
+  PetaboxLoader3.resolve: 83.058 (2)
+  RedisCDXSource: 16.738
+  exclusion.robots.policy: 0.142
+  PetaboxLoader3.datanode: 323.97 (7)
+  esindex: 0.013
+  CDXLines.iter: 39.641 (3)
+  load_resource: 56.698
+  exclusion.robots: 0.156
+*/
